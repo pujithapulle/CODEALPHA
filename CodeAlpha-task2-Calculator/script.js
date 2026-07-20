8714259d6f9029b75da27fa42e81d07987c8593e
pujithapulle <pujithapulle@gmail.com>
2026-07-20 19:50:13 +0530

    Add files via upload

---
+++ CodeAlpha-task2-Calculator/script.js
@@ -0,0 +1,44 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown",function(event){
+
+    let key = event.key;
+
+    if("0123456789+-*/.%".includes(key)){
+        appendValue(key);
+    }
+
+    else if(key==="Enter"){
+        event.preventDefault();
+        calculate();
+    }
+
+    else if(key==="Backspace"){
+        deleteLast();
+    }
+
+    else if(key==="Escape"){
+        clearDisplay();
+    }
+});
